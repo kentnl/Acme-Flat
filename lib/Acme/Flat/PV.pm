560d1529f16caae5e342378c9b79a59dc7a2e321
Kent Fredric <kentfredric@gmail.com>
2015-03-21 09:46:04 +1300

    Build results of d5f9873 (on master)

---
+++ lib/Acme/Flat/PV.pm
@@ -0,0 +1,41 @@
+use 5.006;
+use strict;
+use warnings;
+
+package Acme::Flat::PV;
+
+# ABSTRACT: A Pointer Value ( Generally, for strings )
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+
+use parent 'Acme::Flat::SV';
+use Class::Tiny;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::Flat::PV - A Pointer Value ( Generally, for strings )
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/Acme/Flat/PVIV.pm
@@ -0,0 +1,40 @@
+use 5.006;
+use strict;
+use warnings;
+
+package Acme::Flat::PVIV;
+
+# ABSTRACT: A Value with both String and Integer Parts
+
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
+use parent qw( Acme::Flat::PV Acme::Flat::IV );
+use Class::Tiny;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::Flat::PVIV - A Value with both String and Integer Parts
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
