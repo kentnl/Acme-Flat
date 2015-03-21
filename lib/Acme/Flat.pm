4f02de8998f4428cd7ebc2a0dcc193c67ac7f316
Kent Fredric <kentfredric@gmail.com>
2015-03-21 15:49:44 +1300

    Build results of da8cf37 (on master)

--- lib/Acme/Flat.pm
+++ lib/Acme/Flat.pm
@@ -26,45 +26,6 @@ Acme::Flat - A Pure Perl reimplementation of B Internals
 
 version 0.001000
 
-=head1 DESCRIPTION
-
-This module is mostly a learning experiment to help me understand the mechanics
-behind perl's C<B> internals, and hopefully making more friendly documentation for C<B>
-components in the process.
-
-At present, it only contains the implemented hierarchy present in C<< >5.18 >>, with
-no more than stub classes to represent each C<OP>, each with descriptions.
-
-But it is hoped alone that having a description for each C<OP> gives some improvement on its own.
-
-=head1 NAMING
-
-The name is an approximation of
-
-  Acme::♭
-
-Which was a cutesy way of saying C<B> without saying C<B>
-
-Alas, C<♭> is character C<U+266d>, a I<Symbol>, not an C<AlphaNumeric> included in the
-C<XID_Start> or C<XID_Continue> Regex ranges.
-
-I fully intend to capitalize on that name however in the event that:
-
-=over 4
-
-=item 1. MooseX::Types::Perl::DistName allows symbols in distnames
-
-=item 2. PAUSE itself is proven to accept them.
-
-=item 3. Perl itself supports such a character in identifiers
-
-=back
-
-However, due to L<<
-C<#3> being now required to appease PAUSE Indexing rules
-|http://www.dagolden.com/index.php/2414/this-distribution-name-can-only-be-used-by-users-with-permission/
->>
-
 =head1 AUTHOR
 
 Kent Fredric <kentnl@cpan.org>
