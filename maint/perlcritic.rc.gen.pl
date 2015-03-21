ef9f465f501f198025fc981bc23e25c57f2bda26
Kent Fredric <kentfredric@gmail.com>
2015-03-21 15:21:15 +1300

    Build results of 0af02bd (on master)

--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -18,7 +18,7 @@ use Path::Tiny qw(path);
 my $bundle = create_bundle('Example::Author::KENTNL');
 $bundle->configure;
 
-my @stopwords = (qw(perl reimplementation Metadata LValue ));
+my @stopwords = (qw(perl reimplementation Metadata LValue ithreads Unary SV ));
 for my $wordlist (@stopwords) {
   $bundle->add_or_append_policy_field( 'Documentation::PodSpelling' => ( 'stop_words' => $wordlist ) );
 }
