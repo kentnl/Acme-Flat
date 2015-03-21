1953cd3668d6b78a46e072609571d1576369119f
Kent Fredric <kentfredric@gmail.com>
2015-03-21 15:34:14 +1300

    Build results of da8cf37 (on master)

--- lib/Acme/Flat.pm
+++ lib/Acme/Flat.pm
@@ -37,6 +37,33 @@ no more than stub classes to represent each C<OP>, each with descriptions.
 
 But it is hoped alone that having a description for each C<OP> gives some improvement on its own.
 
+=head1 NAME
+
+The name is an approximation of
+
+  Acme::♭
+
+Which was a cutesy way of saying C<B> without saying C<B>
+
+Alas, C<♭> is character C<U+266d>, a I<Symbol>, not an C<AlphaNumeric> included in the C<XID_Start> or C<XID_Continue> Regex ranges.
+
+I fully intend to capitalize on that name however in the event that:
+
+=over 4
+
+=item 1. MooseX::Types::Perl::DistName allows symbols in distnames
+
+=item 2. PAUSE itself is proven to accept them.
+
+=item 3. Perl itself supports such a character in identifiers
+
+=back
+
+However, due to L<<
+C<#3> being now required to appease PAUSE Indexing rules
+|http://www.dagolden.com/index.php/2414/this-distribution-name-can-only-be-used-by-users-with-permission/
+>>
+
 =head1 AUTHOR
 
 Kent Fredric <kentnl@cpan.org>
